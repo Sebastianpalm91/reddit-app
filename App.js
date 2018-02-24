2962ecff8658c850b3638860876420d5743f2cc6
GabrielAanesen <31622983+GabrielAanesen@users.noreply.github.com>
2018-02-24 02:36:47 +0100

    Revert "fixxed Api"
    This reverts commit 7b5df6aa9e5d040493563df601183044d089225f.

--- App.js
+++ App.js
@@ -7,13 +7,14 @@ import { AppRegistry,
 import ApiContent from './src/components/Api/ApiContent';
 // import Login from './src/components/Login/Login';
 // import Splash from './src/loadingscreen/Splash';
-import QueryInput from './src/components/Api/QueryInput';
+
 export default class App extends React.Component {
+
+
+
   render() {
     return (
       <View style={styles.container}>
-        <QueryInput/>
-        <Text>{QueryInput.passwordInput}</Text>
         <ApiContent/>
       </View>
     );
@@ -23,7 +24,6 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 20,
     backgroundColor: '#EEEEEE',
     justifyContent: 'center',
   },
