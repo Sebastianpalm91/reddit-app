7b5df6aa9e5d040493563df601183044d089225f
GabrielAanesen <31622983+GabrielAanesen@users.noreply.github.com>
2018-02-24 02:24:53 +0100

    fixxed Api

--- App.js
+++ App.js
@@ -7,14 +7,13 @@ import { AppRegistry,
 import ApiContent from './src/components/Api/ApiContent';
 // import Login from './src/components/Login/Login';
 // import Splash from './src/loadingscreen/Splash';
-
+import QueryInput from './src/components/Api/QueryInput';
 export default class App extends React.Component {
-
-
-
   render() {
     return (
       <View style={styles.container}>
+        <QueryInput/>
+        <Text>{QueryInput.passwordInput}</Text>
         <ApiContent/>
       </View>
     );
@@ -24,6 +23,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 20,
     backgroundColor: '#EEEEEE',
     justifyContent: 'center',
   },
--- src/components/Api/ApiContent.js
+++ src/components/Api/ApiContent.js
@@ -3,7 +3,7 @@ import {View,
         FlatList,
         Text,
         Image,
-        Button,
+        Linking,
         } from 'react-native';
 
 export default class ApiContent extends React.Component{
@@ -16,31 +16,25 @@ export default class ApiContent extends React.Component{
   }
 
   fetchData = async () => {
-    const response = await fetch("http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=4GTOUVolpNMX9zDlgN2lRsz65ILvFmVL");
+    const response = await fetch("http://api.giphy.com/v1/gifs/search?q=funny+dog&api_key=4GTOUVolpNMX9zDlgN2lRsz65ILvFmVL");
     const json = await response.json();
     this.setState({data: json.data});
   };
   render (){
     return (
-      <View>
       <FlatList
       data={this.state.data}
       keyExtractor={(x, i) => i}
       renderItem={({item}) =>
+      <View>
          <Image
          style={{width: 100, height: 100}}
          source={{uri:`${item.images.original.url}`}}
          />
-        // <Text>{item.url}</Text>
-        // <Button
-        // onPress={onPressLearnMore}
-        // title="Learn More"
-        // color="#841584"
-        // accessibilityLabel="Learn more about this purple button"
-        // />
+        <Text onPress={ ()=> Linking.openURL(`${item.images.original.url}`)} >Click here for source</Text>
+        </View>
       }
       />
-      </View>
     );
   }
 }
